5c375799ccff983be05df91a5796903b9d9c4a27
Unknown <clara.desmet@student.kuleuven.be>
2019-02-28 19:32:34 +0100

    Indexen!

--- src/ReadInput.java
+++ src/ReadInput.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.ArrayUtil;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -16,16 +18,30 @@ public class ReadInput {
         String[][] fullList = new String[30][]; // the big list, has to have a fixed nb of rows. Need to remove that afterwards maybe?
         int index = 0;
         while (s.hasNextLine()) { // check for next line
-            String[] tempArray = processLine(s.nextLine()); // process the line into an array
+            String[] tempArray = processLine(s.nextLine(),index); // process the line into an array
             fullList[index] = tempArray; // add to the bigger array
             index++; // increment index
         }
         return fullList;
     }
 
-    public String[] processLine(String l) {
-        return l.split(" ");
+    public String[] processLine(String l, int index) {
+        String[] result = l.split(" ");
+        if (index != 0) {
+            String[] indexString = {Integer.toString(index)};
+            return concat(indexString,result);
+        }
+        return result;
 
+
+
+    }
+
+    // Method for concatenating
+    public static <T> T[] concat(T[] first, T[] second) {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
     }
 
     public static String[][] deleteNulls(String[][] list) {
@@ -37,6 +53,11 @@ public class ReadInput {
         }
         return list;
     }
+
+
+//    public static Photo convertToPhoto() {
+//        return null;
+//    }
 
     public static void main(String[] args) throws FileNotFoundException {
         ReadInput r = new ReadInput("./src/a_example.txt");
